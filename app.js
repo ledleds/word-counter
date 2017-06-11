e9547148af02441e3d54c05de2a843171596f972
Vicky Ledsom <victorialedsom@gmail.com>
2017-06-11 11:47:10 +0100

    Create post request for form

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  // res.send('Hello World!')
   res.render("index", { title: 'The index page!' });
-})
+});
+
+app.post("/counter", urlencodedParser, function(req, res) {
+  var text = req.body.textinput;
+  res.send('Got a POST request: ' + text)
+});
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!')
-})
+  console.log('Listening on port 3000!');
+});
